ba9c0edeeb6fc49ae4eedf3e0adbfe52fbf2f623
Sprilloux <alperdemirbilek12@gmail.com>
2024-05-22 20:52:38 +1000

    couple changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Contacts from './Contacts';
 import ContactDetail from './ContactDetail';
 import 'bootstrap/dist/css/bootstrap.min.css';
